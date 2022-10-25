de10d2529b6924134c1c904ffa06e0ec3f182308
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-10-25 21:26:00 +0530

    Add files via upload

---
+++ ifelseDemo.java
@@ -0,0 +1,14 @@
+public class IfelsDemo
+{
+	public static void main (String[] args){
+		int A=10;
+		int B=20;
+		if(A<20){
+		System.out.println("A is TRUE");
+		if(B<10);
+		else{
+			System.out.println("B is FALSE");
+ 	}
+  }
+ }
+}
