14ddeb6f251e835862c2017a9ea38d7c0cdea2d1
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-10-25 21:54:25 +0530

    Add files via upload

---
+++ Switch2Demo.java
@@ -0,0 +1,39 @@
+class Switch
+{
+	public static void main (String[] args)
+	{
+		int x=1;/*✓ char ch="0"
+		switch(ch)*/
+		int y=5;
+		switch (x){
+
+		case 1://Statement
+	System.out.println("Sunday");
+		break;
+		
+		case 2://statement
+	System.out.println("Monday");
+		break;/*it'll use for any looping condition*/
+		
+		case 3:	System.out.println("Tuesday");
+		break;
+		
+		case 4:
+System.out.println("Wednesday");
+break;
+
+case 5:
+System.out.println("Thursday");
+break;
+
+case 6:
+System.out.println("Friday");
+break;
+
+case 7:
+System.out.println("Saturday");
+		default://statement System.out.println("Only 7days in a weak, Sunday will be Holiday");
+ 	 
+   }
+  }
+ }
