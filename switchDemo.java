98bc7ff63f1c92bc7dbe7a7b9ae3a4eded3e02bb
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-10-25 21:52:44 +0530

    Add files via upload

---
+++ switchDemo.java
@@ -0,0 +1,17 @@
+class Switch_Demo1
+{
+	public static void main (String[] args){
+		int x=20;
+		switch (x){
+		case 10:
+		System.out.println("10");
+		break;
+		case 20:
+		System.out.println("20");
+		break;
+		case 30:
+		System.out.println("30");
+		default: System.out.println("Only 3cases will be there");
+	}
+ }
+}
