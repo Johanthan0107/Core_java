d20cc987b6f4ee9cf1007d51ca12a2afe5537d1f
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-10-25 22:12:46 +0530

    Add files via upload

---
+++ whilloop.java
@@ -0,0 +1,13 @@
+/* WHILELOOP:-it's used to iterate over the no of statement multiple times, however if we don't the no of iteration advanced*/
+class whileloop
+{
+	public static void main (String[] args)
+	{
+		int i=0;
+		while (i<10){//condition will check
+		System.out.println(i);
+		i++;//or i=i+2//statement will execute
+	}
+ }
+}
+//O/p is infinity 
