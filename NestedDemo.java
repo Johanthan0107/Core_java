aabf0dedf3c4c0d2f3fc74da0d26f21d4f1a1445
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-10-25 21:41:06 +0530

    Add files via upload

---
+++ NestedDemo.java
@@ -0,0 +1,24 @@
+public class NestedifDemo{
+	public static void main (String[] args){
+		
+String round1Status="Pass";
+ if(round1Status == "Pass") {
+  System.out.println("✔️You have cleared round one so,😍wait for round2");
+
+String round2Status="Pass"; 
+ if(round2Status == "Pass") {
+  System.out.println("✔️you have cleared round two also so, 😍wait for HR round only");
+ 
+String round3Status="Fail";
+ if(round3Status == "Fail") {
+  System.out.println("❌ Sorry You're not cleared HR round so, Go to your 🏘️home");
+ }
+ 
+ else{
+
+ System.out.println("You can goto home");
+    }
+   }
+  }
+ }
+}
