775df5416593697bcdf6343a519fce6033d34fbe
Johanthan0107 <116352769+Johanthan0107@users.noreply.github.com>
2022-10-25 20:26:14 +0530

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,17 @@
+
+public class Prime_Even_Odd_Number
+{
+	public static void main (String[] args)
+	{
+		int num=50;
+		if( num%5==0)
+		{
+			System.out.println("Entern no is even");
+			System.out.println("Enter no is prime");
+			System.out.println("Enter no is naturalno");
+			}
+			else{
+				System.out.println("Entern no is odd");
+		}
+	}
+}
