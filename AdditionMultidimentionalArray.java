0d68e1a9c3767baf230e28563a97a992288cbcde
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-10-27 17:45:28 +0530

    Add files via upload

---
+++ AdditionMultidimentionalArray.java
@@ -0,0 +1,17 @@
+/* Multidimensional Array:- it's data is stored in row And column based index(matrixform) */
+class AdditionArray
+{
+	public static void main (String[] args)
+	{
+		int a[][]={{1,2,3},{4,5,6}};
+		int b[][]={{7,8,9,},{10,11,12}};
+		int c[][]=new int[2][3];
+		for (int i=0;i<2;i++){
+			for (int j=0;j<3;j++){
+				c[i][j]=a[i][j]+b[i][j];
+		System.out.println(c[i][j]+" ");
+		}
+		System.out.println();
+	}
+ }
+}
