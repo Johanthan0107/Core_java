c8656a7cd0c2ba0d90918db16f8cef833a137e72
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-07 18:24:14 +0530

    Add files via upload

---
+++ swap2.java
@@ -0,0 +1,10 @@
+class Swaping { 
+	public static void main(String[] args) { 
+ int a, b; 
+ a = 15; 
+ b = 10; System.out.println("Before swapping : a,b = "+a+", "+b+" "); 
+ a = a + b;
+ b = a - b; 
+ a = a - b; System.out.println("After swapping : a,b = "+a+", "+b+" ");//we can use + + b(include space in mid of pulse
+}
+}
