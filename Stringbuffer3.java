295903b18c48c41e72fb0f0ced0710522003b2e2
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-10 18:53:45 +0530

    Add files via upload

---
+++ Stringbuffer3.java
@@ -0,0 +1,12 @@
+//package com.jojuskills;
+public class Test {
+	public static void main(String[] raj){
+		StringBuffer sb=new StringBuffer("Johnathan");
+		System.out.println(sb);
+		System.out.println(sb.append("Java"));
+		System.out.println(sb.replace(2, 3, "Joshua"));
+		System.out.println(sb.delete(2,3));
+		System.out.println(sb.reverse());
+		
+	}
+}
