5ea3a995fd40e60caaca643bfcd9d6dd7fbcfab3
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-10 18:51:04 +0530

    Add files via upload

---
+++ stringarrayreturn.java
@@ -0,0 +1,9 @@
+class String_Test {
+public static void main(String args[]) {
+	String s="My name is Joshua";
+	String[] strArray=s.split(" ");
+	for(int i=strArray.length-1; i>=0; i--)
+	System.out.print(strArray[i]+" ");
+	
+	}
+}
