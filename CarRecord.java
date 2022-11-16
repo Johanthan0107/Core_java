5783fe01004486449874664b4b42a1769dd1705e
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-16 17:34:46 +0530

    Update CarRecord.java

--- CarRecord.java
+++ CarRecord.java
@@ -7,7 +7,11 @@ public static void main(String[] args){
 	Car c1=new Car(70,"Jaguar",5000000f,"Sports");
         Car C2=new Car( 80,"Odi",7000000f,"Benz");
 	System.out.println(c);
+//Car[Carinches=100, Name=BMW, Prize=6000000.0, Type=Benz]
 	System.out.println(c1);
-        System.out.println(C2);
+//Car[Carinches=70, Name=Jaguar, Prize=5000000.0, Type=Sports]
+    System.out.println(C2);
+//Car[Carinches=80, Name=Odi, Prize=7000000.0, Type=Benz]
+
 }
 }
