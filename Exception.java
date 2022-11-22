605d0ea17d8b07c15c975e09c33c87c2110a4f64
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-22 14:55:58 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,18 @@
+class Exception {
+public static void main(String[] arg) {
+var x=75;/*Warning\Error
+
+Main.java:3: error: cannot find symbol
+var x=75;
+^
+  symbol:   class var
+  location: class*/
+  	
+var s="Hello";/*error: cannot find symbol
+var s="Hello";
+^
+  symbol:   class var
+  location: class Exception*/
+System.out.println(" "+x+" "+s);
+ }//Var is a keyword, used for ducting content of the datatype
+}
