7555fdf17cee261512c7d9353f7b6cfce7ee660b
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-22 14:56:39 +0530

    Add files via upload

---
+++ CheckedExeption.java
@@ -0,0 +1,34 @@
+//import java.io.*;
+class CheckedExceptionDemo {
+public static void main(String args[]) throws FileNotFoundException,IOException {
+FileInputStream fis=null;
+fis=new FileInputStream("D:/JavaProgram/Welcome.txt");
+int i;
+while((i=fis.read()) != -1) {
+System.out.print((char)i+""); }
+fis.close();
+ }
+}
+/*Warning\Error
+
+Main.java:3: error: cannot find symbol
+public static void main(String args[]) throws FileNotFoundException,IOException {
+                                              ^
+  symbol:   class FileNotFoundException
+  location: class CheckedExceptionDemo
+Main.java:3: error: cannot find symbol
+public static void main(String args[]) throws FileNotFoundException,IOException {
+                                                                    ^
+  symbol:   class IOException
+  location: class CheckedExceptionDemo
+Main.java:4: error: cannot find symbol
+FileInputStream fis=null;
+^
+  symbol:   class FileInputStream
+  location: class CheckedExceptionDemo
+Main.java:5: error: cannot find symbol
+fis=new FileInputStream("D:/JavaProgram/Welcome.txt");
+        ^
+  symbol:   class FileInputStream
+  location: class CheckedExceptionDemo
+4 errors*/
