16823f17aa0c72eea1a246c9e2d3522ca2326410
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-12-02 14:24:28 +0530

    Add files via upload

---
+++ Collectionstream2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.Collectors;
+public class StreamDemo1 {
+	
+public static void main(String args[]){
+	
+ArrayList<Integer> marks=new ArrayList<Integer>();
+
+marks.add(60);
+marks.add(34);
+marks.add(25);
+marks.add(70);
+marks.add(40);
+marks.add(22);
+System.out.println(marks);
+//filter the pass marks
+
+List<Integer> Failmark=marks.stream().filter(m->m<=35).collect(Collectors.toList());
+System.out.println(Failmark);  }
+}
