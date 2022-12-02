666c1ada479606687fbf8bb1cdcc354145ed28b9
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-12-02 14:30:29 +0530

    Add files via upload

---
+++ Interfaceswithabstract.java
@@ -0,0 +1,23 @@
+//Interfaces:- An interface is a completely "abstract class" that is used to group related methods with empty bodies
+
+interface Animal {
+  public void animalSound(); // interface method (does not have a body)
+  public void sleep(); // interface method (does not have a body)
+}
+
+class Pig implements Animal {
+  public  void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Pig myPig = new Pig();
+    myPig.animalSound();
+    myPig.sleep();
+ }
+}
